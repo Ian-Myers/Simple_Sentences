a5f68c6c6474c266b434e95f49505981c357ad97
Ian Myers <ianmyers12@gmail.com>
2018-08-24 13:59:42 -0400

    Initial

---
+++ Simple_Sentences/Simple_Sentences/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Sentences
+{
+    class Program
+    {
+        static void Main()
+        {
+            int pronounCount = Enum.GetNames(typeof(SubjectPronoun)).Length; //Counts the amount of enums.
+            int pastSimpleIrregularVerbCount = Enum.GetNames(typeof(PastSimpleIrregularVerb)).Length;
+
+            Random rnd = new Random();
+            int pronounID = rnd.Next(pronounCount); //Assigns a random integer no higher than the number of enums referenced
+            int pastSimpleIrregularVerbID = rnd.Next(pastSimpleIrregularVerbCount);
+
+            SubjectPronoun pronoun = new SubjectPronoun();
+            pronoun = (SubjectPronoun)pronounID;
+
+            PastSimpleIrregularVerb verb = new PastSimpleIrregularVerb();
+            verb = (PastSimpleIrregularVerb)pastSimpleIrregularVerbID;
+
+            string sentence = (pronoun+ " " +verb);
+
+            Console.WriteLine(sentence);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Simple_Sentences/Simple_Sentences/SentenceStructure.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Sentences
+{
+    public class SentenceStructure
+    {
+        public SubjectPronoun SubjectPronoun { get; set; }
+        public PastSimpleIrregularVerb PastSimpleIrregularVerb { get; set; }
+    }
+    public enum SubjectPronoun
+    {
+        I,
+        He,
+        She,
+        We,
+        You,
+        They,
+        It
+    }
+
+    public enum PastSimpleIrregularVerb //For now this will only contain verbs that make sense in a two-word sentence.
+    {
+        Ran,
+        Woke,
+        Dove,
+        Drove,
+        Drank,
+        Ate,
+        Flew,
+        Froze,
+        Grew,
+        Hid,
+        Hurt,
+        Knew,
+        Left,
+        Paid,
+        Read,
+        Rang,
+        Said,
+        Sang,
+        Sat,
+        Slept,
+        Spoke,
+        Stood,
+        Thought,
+        Understood,
+        Won
+    }
+}
